b0aa0af994ab8ea306e904a3c8d7dae791262f97
seunwoo kang <kangsw0304@naver.com>
2024-10-16 18:35:03 +0900

    upload NN ipynb workflow

--- curate_training_test_data.py
+++ curate_training_test_data.py
@@ -5,6 +5,7 @@ from create_sequences import create_sequences
 def curate_training_test_data(df_all, Date_col_name = 'Date',
                               sequence_length = 7, 
                               test_date= '2024-07-01', 
+                              flatten = False,
                               predictors_lst = ['EUA', 'Oil', 'Coal', 'NG', 'USEU', 'S&P_clean', 'DAX']):
     test_date = pd.to_datetime(test_date)
 
@@ -22,7 +23,7 @@ def curate_training_test_data(df_all, Date_col_name = 'Date',
 
     train_data_scaled = scaler.transform(train_data)
     test_data_scaled  = scaler.transform(test_data)
-    X_train, y_train = create_sequences(train_data_scaled, sequence_length, flatten = True) # LSTM should be flatten = False
-    X_test, y_test = create_sequences(test_data_scaled, sequence_length, flatten = True)
+    X_train, y_train = create_sequences(train_data_scaled, sequence_length, flatten = flatten) # LSTM should be flatten = False
+    X_test, y_test = create_sequences(test_data_scaled, sequence_length, flatten = flatten)
     return X_train, y_train, X_test, y_test, scaler
 
