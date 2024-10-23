2e2abe9c3204869869d7916feeb8b8f5b8805c1c
Geomodeller <honggeun.jo@inha.ac.kr>
2024-10-23 17:02:00 +0900

    update

--- script/create_sequences.py
+++ script/create_sequences.py
@@ -17,6 +17,23 @@ def create_sequences(data: np.array, seq_length: int, flatten = False):
     if flatten:
         X = np.array(X)
         y = np.array(y)
-        return X.reshape(X.shape[0],-1), y.reshape(y.shape[0],-1)
+        return X.reshape(X.shape[0],-1).astype('float16'), y.reshape(y.shape[0],-1).astype('float16')
     else:
-        return np.array(X), np.array(y)
+        return np.array(X).astype('float16'), np.array(y).astype('float16')
+
+def create_sequences_many(data: np.array, 
+                           seq_length_input: int=3, 
+                           seq_length_out: int=3, 
+                           flatten: bool = False):
+    X, y = [], []
+    for i in range(len(data)- (seq_length_input + seq_length_out) + 1):
+        feature = data[i:i+seq_length_input]
+        target = data[i+seq_length_input:i+seq_length_input+seq_length_out]
+        X.append(feature)
+        y.append(target)
+    if flatten:
+        X = np.array(X)
+        y = np.array(y)
+        return X.reshape(X.shape[0],-1).astype('float16'), y.reshape(y.shape[0],-1).astype('float16')
+    else:
+        return np.array(X).astype('float16'), np.array(y).astype('float16')
--- script/curate_training_test_data.py
+++ script/curate_training_test_data.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from script.create_sequences import create_sequences
+from script.create_sequences import create_sequences, create_sequences_many
 
 def curate_training_test_data(df_all, Date_col_name = 'Date',
                               sequence_length = 7, 
@@ -9,8 +9,6 @@ def curate_training_test_data(df_all, Date_col_name = 'Date',
                               flatten = False,
                               predictors_lst = ['EUA', 'Oil', 'Coal', 'NG', 'USEU', 'S&P_clean', 'DAX']):
     test_date = pd.to_datetime(test_date)
-
-
 
     test_overlap_time = pd.to_timedelta(sequence_length+1, unit = 'day')
 
@@ -28,3 +26,27 @@ def curate_training_test_data(df_all, Date_col_name = 'Date',
     X_test, y_test = create_sequences(test_data_scaled, sequence_length, flatten = flatten)
     return X_train, y_train, X_test, y_test, scaler
 
+
+def curate_training_test_data_many(df_all, Date_col_name = 'Date',
+                              sequence_length_input = 7,
+                              sequence_length_out = 7, 
+                              test_date= '2024-07-01', 
+                              flatten = False,
+                              predictors_lst = ['EUA', 'Oil', 'Coal', 'NG', 'USEU', 'S&P_clean', 'DAX']):
+    test_date = pd.to_datetime(test_date)
+
+    test_overlap_time = pd.to_timedelta(sequence_length_input+1, unit = 'day')
+
+    df_train = df_all[df_all[Date_col_name] < test_date].reset_index(drop=True)
+    df_test  = df_all[df_all[Date_col_name] > test_date - test_overlap_time].reset_index(drop=True)
+
+    train_data = df_train[predictors_lst].values 
+    test_data  = df_test[predictors_lst].values
+    scaler = StandardScaler()
+    scaler.fit(train_data)
+
+    train_data_scaled = scaler.transform(train_data)
+    test_data_scaled  = scaler.transform(test_data)
+    X_train, y_train = create_sequences_many(train_data_scaled, sequence_length_input, sequence_length_out, flatten = flatten) # LSTM should be flatten = False
+    X_test, y_test = create_sequences_many(test_data_scaled, sequence_length_input, sequence_length_out, flatten = flatten)
+    return X_train, y_train, X_test, y_test, scaler
